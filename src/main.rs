047f76036d93904b1daa9cf282f48aa91096fbaa
banpapi <banpapi8787@gmail.com>
2019-08-19 04:56:40 +0200

    create main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use new_minigrep::config::Config;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+
+    let config = Config::new(std::env::args())
+        .unwrap_or_else(|error|{
+            println!("Error in arguments parsing: {}", error);
+            process::exit(1);
+        });
+
+    if let Err(e) = new_minigrep::run::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
